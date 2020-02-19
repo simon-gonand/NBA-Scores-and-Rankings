e310749e36fed7dd23a66f3665538b8a0f2f3df2
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-19 14:26:23 +0000

    improve localShow function

--- com/NBA_Rankings_Scores_Project/Tree/Tree.java
+++ com/NBA_Rankings_Scores_Project/Tree/Tree.java
@@ -1,7 +1,6 @@
 package com.NBA_Rankings_Scores_Project.Tree;
 
-import com.NBA_Rankings_Scores_Project.Models.Models;
-import com.NBA_Rankings_Scores_Project.Models.Season;
+import com.NBA_Rankings_Scores_Project.Models.*;
 
 public class Tree {
     private TreeNode root;
@@ -23,12 +22,26 @@ public class Tree {
     private void localShow(TreeNode node) {
         if (node.equals(null)) return;
         for (TreeNode subNode : node.getChilds()){
-            System.out.println("Node : " + subNode.getData().getName());
+            if (subNode.getData().getClass().equals(Conference.class)) {
+                System.out.println("---------------------------------------------------------");
+                System.out.println("Conference : " + subNode.getData().getName());
+            }
+
+            if (subNode.getData().getClass().equals(TeamModel.class)) {
+                System.out.println("-------------------------------");
+                System.out.println("Team : " + subNode.getData().getName());
+                System.out.println("----------------");
+            }
+
+            if (subNode.getData().getClass().equals(PlayerModel.class)) {
+                System.out.println("Player : " + subNode.getData().getName());
+            }
             localShow(subNode);
         }
     }
 
     public void show(){
-        localShow(root);
+        System.out.println("Season : " + this.root.getData().getName());
+        localShow(this.root);
     }
 }

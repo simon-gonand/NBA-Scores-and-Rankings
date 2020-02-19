a8437f67fb3f252f9b209303ac01e5a993428037
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-19 13:53:33 +0000

    implements tree

--- com/NBA_Rankings_Scores_Project/Tree/Tree.java
+++ com/NBA_Rankings_Scores_Project/Tree/Tree.java
@@ -1,4 +1,33 @@
 package com.NBA_Rankings_Scores_Project.Tree;
 
+import com.NBA_Rankings_Scores_Project.Models.Models;
+import com.NBA_Rankings_Scores_Project.Models.Season;
+
 public class Tree {
+    private TreeNode root;
+
+    public Tree(Season season) {
+        root = new TreeNode (season);
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    void add(TreeNode node, Models val){
+        TreeNode tmp = new TreeNode(val);
+        node.addChilds(tmp);
+    }
+
+    private void localShow(TreeNode node) {
+        if (node.equals(null)) return;
+        for (TreeNode subNode : node.getChilds()){
+            System.out.println("Node : " + subNode.getData().getName());
+            localShow(subNode);
+        }
+    }
+
+    public void show(){
+        localShow(root);
+    }
 }
--- com/NBA_Rankings_Scores_Project/Tree/TreeNode.java
+++ com/NBA_Rankings_Scores_Project/Tree/TreeNode.java
@@ -1,4 +1,30 @@
 package com.NBA_Rankings_Scores_Project.Tree;
 
+import com.NBA_Rankings_Scores_Project.Models.Models;
+
+import java.util.List;
+
 public class TreeNode {
+    private Models data;
+    private List<TreeNode> childs;
+
+    public TreeNode(Models val){
+        data = val;
+    }
+
+    public Models getData() {
+        return data;
+    }
+
+    public List<TreeNode> getChilds() {
+        return childs;
+    }
+
+    public void setData(Models data) {
+        this.data = data;
+    }
+
+    public void addChilds(TreeNode child) {
+        this.childs.add(child);
+    }
 }

8f44868403596ec1200013228f32bea44c6dd6da
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-19 14:26:42 +0000

    example of completing Tree

--- com/NBA_Rankings_Scores_Project/Parser.java
+++ com/NBA_Rankings_Scores_Project/Parser.java
@@ -1,5 +1,8 @@
 package com.NBA_Rankings_Scores_Project;
 
+import com.NBA_Rankings_Scores_Project.Models.*;
+import com.NBA_Rankings_Scores_Project.Tree.Tree;
+import com.NBA_Rankings_Scores_Project.Tree.TreeNode;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
@@ -12,29 +15,37 @@ public class Parser {
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
-            java.lang.System.out.println("Root element : " + doc.getDocumentElement().getNodeName() + " " + doc.getDocumentElement().getAttribute("name"));
-            NodeList nodeList = doc.getElementsByTagName("Conference");
-            java.lang.System.out.println("-------------------------------");
 
-            for (int i = 0; i < nodeList.getLength(); ++i){
-                Node node = nodeList.item(i);
-                java.lang.System.out.println("Current : " + node.getNodeName());
-                if (node.getNodeType() == Node.ELEMENT_NODE){
-                    Element element = (Element) node;
-                    java.lang.System.out.println("name : " + element.getAttribute("name"));
-                    NodeList subNodeList = element.getElementsByTagName("Team");
-                    for (int j = 0; j < subNodeList.getLength(); ++j){
-                        Node subNode = subNodeList.item(j);
-                        java.lang.System.out.println("----------------");
-                        if (subNode.getNodeType() == Node.ELEMENT_NODE){
-                            Element subElement = (Element) subNode;
-                            java.lang.System.out.println(subElement.getNodeName() + " " + j + " : " + subElement.getAttribute("name"));
-                            java.lang.System.out.println("Head coach : " + subElement.getAttribute("headCoach"));
+            Season season = new Season(doc.getDocumentElement().getAttribute("name"));
+            Tree tree = new Tree(season);
+            NodeList confList = doc.getElementsByTagName("Conference");
+
+            for (int i = 0; i < confList.getLength(); ++i){
+                Node confNode = confList.item(i);
+                if (confNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element confElement = (Element) confNode;
+                    TreeNode conference = tree.add(tree.getRoot(), new Conference(confElement.getAttribute("name")));
+
+                    NodeList teamsList = confElement.getElementsByTagName("Team");
+                    for (int j = 0; j < confList.getLength(); ++j){
+                        Node teamNode = teamsList.item(j);
+                        if (teamNode.getNodeType() == Node.ELEMENT_NODE){
+                            Element teamElement = (Element) teamNode;
+                            TreeNode team = tree.add(conference, new TeamModel(teamElement.getAttribute("name")));
+
+                            NodeList playersList = teamElement.getElementsByTagName("Player");
+                            for (int k = 0; k < playersList.getLength(); ++k){
+                                Node playerNode = playersList.item(k);
+                                if (playerNode.getNodeType() == Node.ELEMENT_NODE){
+                                    Element playerElement = (Element) playerNode;
+                                    TreeNode player = tree.add(team, new PlayerModel(playerElement.getAttribute("name")));
+                                }
+                            }
                         }
                     }
                 }
-                java.lang.System.out.println("-------------------------------");
             }
+           tree.show();
         } catch (Exception e){
             e.printStackTrace();
         }

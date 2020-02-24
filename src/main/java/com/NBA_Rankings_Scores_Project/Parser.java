5399536c35e02add8b2003cc667065d56e263853
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-02-24 12:07:24 +0000

    organize class

--- src/main/java/com/NBA_Rankings_Scores_Project/Parser.java
+++ src/main/java/com/NBA_Rankings_Scores_Project/Parser.java
@@ -8,9 +8,19 @@ import javax.xml.parsers.*;
 import java.io.*;
 
 public class Parser {
-    public Parser (){
+    public File inputFile;
+    public Parser (String fileName){
         try {
-            File inputFile = new File ("src/Datas/Season_19_20.xml");
+            inputFile = new File (fileName);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public Tree getTree() {
+
+        try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
@@ -18,36 +28,52 @@ public class Parser {
 
             Season season = new Season(doc.getDocumentElement().getAttribute("name"));
             Tree tree = new Tree(season);
-            NodeList confList = doc.getElementsByTagName("Conference");
 
-            for (int i = 0; i < confList.getLength(); ++i){
-                Node confNode = confList.item(i);
-                if (confNode.getNodeType() == Node.ELEMENT_NODE){
-                    Element confElement = (Element) confNode;
-                    TreeNode conference = tree.add(tree.getRoot(), new Conference(confElement.getAttribute("name")));
+            tree = getConferences(doc, tree);
 
-                    NodeList teamsList = confElement.getElementsByTagName("Team");
-                    for (int j = 0; j < confList.getLength(); ++j){
-                        Node teamNode = teamsList.item(j);
-                        if (teamNode.getNodeType() == Node.ELEMENT_NODE){
-                            Element teamElement = (Element) teamNode;
-                            TreeNode team = tree.add(conference, new TeamModel(teamElement.getAttribute("name")));
-
-                            NodeList playersList = teamElement.getElementsByTagName("Player");
-                            for (int k = 0; k < playersList.getLength(); ++k){
-                                Node playerNode = playersList.item(k);
-                                if (playerNode.getNodeType() == Node.ELEMENT_NODE){
-                                    Element playerElement = (Element) playerNode;
-                                    TreeNode player = tree.add(team, new PlayerModel(playerElement.getAttribute("name")));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-           tree.show();
-        } catch (Exception e){
+            return tree;
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return new Tree(new Season("void season"));
+    }
+
+    private Tree getConferences(Document doc, Tree tree){
+        NodeList confList = doc.getElementsByTagName("Conference");
+
+        for (int i = 0; i < confList.getLength(); ++i) {
+            Node confNode = confList.item(i);
+            if (confNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element confElement = (Element) confNode;
+                TreeNode conference = tree.add(tree.getRoot(), new Conference(confElement.getAttribute("name")));
+                tree = getTeams(conference, confElement, tree);
+            }
+        }
+        return tree;
+    }
+
+    private Tree getTeams (TreeNode conference, Element confElement, Tree tree){
+        NodeList teamsList = confElement.getElementsByTagName("Team");
+        for (int j = 0; j < teamsList.getLength(); ++j) {
+            Node teamNode = teamsList.item(j);
+            if (teamNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element teamElement = (Element) teamNode;
+                TreeNode team = tree.add(conference, new TeamModel(teamElement.getAttribute("name")));
+                tree = getPlayers(team, teamElement, tree);
+            }
+        }
+        return tree;
+    }
+
+    private Tree getPlayers (TreeNode team, Element teamElement, Tree tree){
+        NodeList playersList = teamElement.getElementsByTagName("Player");
+        for (int k = 0; k < playersList.getLength(); ++k){
+            Node playerNode = playersList.item(k);
+            if (playerNode.getNodeType() == Node.ELEMENT_NODE){
+                Element playerElement = (Element) playerNode;
+                TreeNode player = tree.add(team, new PlayerModel(playerElement.getAttribute("name")));
+            }
+        }
+        return tree;
     }
 }

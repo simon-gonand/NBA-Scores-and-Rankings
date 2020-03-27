8c535f9793c7ecfcf3a2a688e0f5666b416c68e2
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-03-27 12:00:03 +0000

    all info tested

--- src/test/java/com/NBA_Rankings_Scores_Project/ParserTest.java
+++ src/test/java/com/NBA_Rankings_Scores_Project/ParserTest.java
@@ -1,5 +1,6 @@
 package com.NBA_Rankings_Scores_Project;
 
+import com.NBA_Rankings_Scores_Project.Models.PlayerModel;
 import com.NBA_Rankings_Scores_Project.Models.TeamModel;
 import com.NBA_Rankings_Scores_Project.Tree.*;
 import org.junit.jupiter.api.*;
@@ -52,22 +53,95 @@ public class ParserTest {
     public void teamInfoTest() {
         TeamModel teamWestModel1 = (TeamModel) teamWest1.getData();
         TeamModel teamWestModel2 = (TeamModel) teamWest2.getData();
-        assertEquals("Dallas Mavericks", teamWestModel1.getName(), "Team must be Dallas Mavericks");
-        assertEquals("Los Angeles Clippers", teamWestModel2.getName(), "Team must be Los Angeles Clippers");
+        assertEquals("Dallas Mavericks",
+                teamWestModel1.getName(),
+                "Team's name must be Dallas Mavericks");
+        assertEquals("Rick Carlisle",
+                teamWestModel1.getHeadCoach(),
+                "Team's head coach must be Rick Carlisle");
+
+        assertEquals("Los Angeles Clippers",
+                teamWestModel2.getName(),
+                "Team must be Los Angeles Clippers");
+
+        assertEquals("Doc Rivers",
+                teamWestModel2.getHeadCoach(),
+                "Team must be Doc Rivers");
 
         TeamModel teamEastModel1 = (TeamModel) teamEast1.getData();
         TeamModel teamEastModel2 = (TeamModel) teamEast2.getData();
-        assertEquals("Milwaukee Bucks", teamEastModel1.getName(), "Team must be Milwaukee Bucks");
-        assertEquals("Milwaukee Bucks", teamEastModel1.getName(), "Team must be Milwaukee Bucks");
-        assertEquals("New Orleans Pelicans", teamEastModel2.getName(), "Team must be New Orleans Pelicans");
+        assertEquals("Milwaukee Bucks",
+                teamEastModel1.getName(),
+                "Team'name must be Milwaukee Bucks");
+        assertEquals("Mike Budenholzer",
+                teamEastModel1.getHeadCoach(),
+                "Team's head coach must be Mike Budenholzer");
+        assertEquals("New Orleans Pelicans",
+                teamEastModel2.getName(),
+                "Team's name must be New Orleans Pelicans");
+        assertEquals("Alvin Gentry",
+                teamEastModel2.getHeadCoach(),
+                "Team's head coach must be Alvin Gentry");
     }
 
     @Test
     public void playerInfoTest() {
-        assertEquals("J.J.",playerTeamWest1.getData().getName(), "Player must be J.J.");
-        assertEquals("Patrick",playerTeamWest2.getData().getName(), "Player must be Patrick");
+        PlayerModel playerModelTeamWest1 = (PlayerModel) playerTeamWest1.getData();
+        assertEquals("J.J.", playerModelTeamWest1.getName(), "Player's name must be J.J.");
+        assertEquals("Barea", playerModelTeamWest1.getSurname(), "Player's surname must be Barea");
+        assertEquals(PlayerModel.Position.GUARD,
+                playerModelTeamWest1.getPosition(),
+                "Player's position must be Guard");
+        assertEquals(5, playerModelTeamWest1.getNumber(), "Player's number must be 5");
+        assertEquals(35, playerModelTeamWest1.getAge(), "Player's age must be 35");
+        assertEquals("Puerto Rican",
+                playerModelTeamWest1.getNationality(),
+                "Player's nationality must be Puerto Rican");
+        assertEquals(0, playerModelTeamWest1.getID(), "Player's ID must be 0");
 
-        assertEquals("Giannis",playerTeamEast1.getData().getName(), "Player must be Giannis");
-        assertEquals("Nickeil",playerTeamEast2.getData().getName(), "Player must be Nickeil");
+        PlayerModel playerModelTeamWest2 = (PlayerModel) playerTeamWest2.getData();
+        assertEquals("Patrick", playerModelTeamWest2.getName(), "Player's name must be Patrick");
+        assertEquals("Beverley",
+                playerModelTeamWest2.getSurname(),
+                "Player's surname must be Beverley");
+        assertEquals(PlayerModel.Position.SHOOTING_GUARD,
+                playerModelTeamWest2.getPosition(),
+                "Player's position must be Shooting Guard");
+        assertEquals(31, playerModelTeamWest2.getNumber(), "Player's number must be 31");
+        assertEquals(27, playerModelTeamWest2.getAge(), "Player's age must be 27");
+        assertEquals("American",
+                playerModelTeamWest2.getNationality(),
+                "Player's nationality must be American");
+        assertEquals(0, playerModelTeamWest2.getID(), "Player's ID must be 0");
+
+        PlayerModel playerModelTeamEast1 = (PlayerModel) playerTeamEast1.getData();
+        assertEquals("Giannis", playerModelTeamEast1.getName(), "Player's name must be Giannis");
+        assertEquals("Antetokounmpo",
+                playerModelTeamEast1.getSurname(),
+                "Player's surname must be Antetokounmpo");
+        assertEquals(PlayerModel.Position.POWER_FORWARD,
+                playerModelTeamEast1.getPosition(),
+                "Player's position must be Power Forward");
+        assertEquals(34, playerModelTeamEast1.getNumber(), "Player's number must be 34");
+        assertEquals(25, playerModelTeamEast1.getAge(), "Player's age must be 25");
+        assertEquals("Greek",
+                playerModelTeamEast1.getNationality(),
+                "Player's nationality must be Greek");
+        assertEquals(0, playerModelTeamEast1.getID(), "Player's ID must be 0");
+
+        PlayerModel playerModelTeamEast2 = (PlayerModel) playerTeamEast2.getData();
+        assertEquals("Nickeil", playerModelTeamEast2.getName(), "Player's name must be Nickeil");
+        assertEquals("Alexander-Walker",
+                playerModelTeamEast2.getSurname(),
+                "Player's surname must be Alexander-Walker");
+            assertEquals(PlayerModel.Position.CENTER,
+                    playerModelTeamEast2.getPosition(),
+                    "Player's position must be Center");
+        assertEquals(0, playerModelTeamEast2.getNumber(), "Player's number must be 0");
+        assertEquals(21, playerModelTeamEast2.getAge(), "Player's age must be 21");
+        assertEquals("Canadian",
+                playerModelTeamEast2.getNationality(),
+                "Player's nationality must be Canadian");
+        assertEquals(0, playerModelTeamEast2.getID(), "Player's ID must be 0");
     }
 }

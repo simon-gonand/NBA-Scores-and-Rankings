83c7cd1c362eede00b3276067221549e4b7a997f
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-03-27 11:13:09 +0000

    make several test functions instead of one

--- src/test/java/com/NBA_Rankings_Scores_Project/ParserTest.java
+++ src/test/java/com/NBA_Rankings_Scores_Project/ParserTest.java
@@ -6,40 +6,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
     private static Tree tree;
+    private static TreeNode confWest;
+    private static TreeNode confEast;
+    private static TreeNode teamWest1;
+    private static TreeNode teamWest2;
+    private static TreeNode teamEast1;
+    private static TreeNode teamEast2;
+    private static TreeNode playerTeamWest1;
+    private static TreeNode playerTeamWest2;
+    private static TreeNode playerTeamEast1;
+    private static TreeNode playerTeamEast2;
 
     @BeforeAll
     public static void setup(){
         Parser parser = new Parser("src/main/resources/Test_Datas.xml");
         tree = parser.getTree();
+
+        confWest = tree.getRoot().getChilds().get(0);
+        confEast = tree.getRoot().getChilds().get(1);
+
+        teamWest1 = confWest.getChilds().get(0);
+        teamWest2 = confWest.getChilds().get(1);
+        teamEast1 = confEast.getChilds().get(0);
+        teamEast2 = confEast.getChilds().get(1);
+
+        playerTeamWest1 = teamWest1.getChilds().get(0);
+        playerTeamWest2 = teamWest2.getChilds().get(0);
+        playerTeamEast1 = teamEast1.getChilds().get(0);
+        playerTeamEast2 = teamEast2.getChilds().get(0);
     }
 
     @Test
-    public void generalInfoTest(){
+    public void seasonInfoTest() {
+        assertEquals("2019/20", tree.getRoot().getData().getName(), "Season must be 2019/20");
+    }
 
-        assertEquals("2019/20",tree.getRoot().getData().getName(), "Season must be 2019/20");
+    @Test
+    public void conferenceInfoTest() {
+        assertEquals("West", confWest.getData().getName(), "Conference must be West");
+        assertEquals("East", confEast.getData().getName(), "Conference must be East");
+    }
 
-        TreeNode confWest = tree.getRoot().getChilds().get(0);
-        TreeNode confEast = tree.getRoot().getChilds().get(1);
-        assertEquals("West",confWest.getData().getName(), "Conference must be West");
-        assertEquals("East",confEast.getData().getName(), "Conference must be East");
+    @Test
+    public void teamInfoTest() {
+        assertEquals("Dallas Mavericks", teamWest1.getData().getName(), "Team must be Dallas Mavericks");
+        assertEquals("Los Angeles Clippers", teamWest2.getData().getName(), "Team must be Los Angeles Clippers");
 
-        TreeNode teamWest1 = confWest.getChilds().get(0);
-        TreeNode teamWest2 = confWest.getChilds().get(1);
-        assertEquals("Dallas Mavericks",teamWest1.getData().getName(), "Team must be Dallas Mavericks");
-        assertEquals("Los Angeles Clippers",teamWest2.getData().getName(), "Team must be Los Angeles Clippers");
+        assertEquals("Milwaukee Bucks", teamEast1.getData().getName(), "Team must be Milwaukee Bucks");
+        assertEquals("New Orleans Pelicans", teamEast2.getData().getName(), "Team must be New Orleans Pelicans");
+    }
 
-        TreeNode teamEast1 = confEast.getChilds().get(0);
-        TreeNode teamEast2 = confEast.getChilds().get(1);
-        assertEquals("Milwaukee Bucks",teamEast1.getData().getName(), "Team must be Milwaukee Bucks");
-        assertEquals("New Orleans Pelicans",teamEast2.getData().getName(), "Team must be New Orleans Pelicans");
-
-        TreeNode playerTeamWest1 = teamWest1.getChilds().get(0);
-        TreeNode playerTeamWest2 = teamWest2.getChilds().get(0);
+    @Test
+    public void playerInfoTest() {
         assertEquals("J.J.",playerTeamWest1.getData().getName(), "Player must be J.J.");
         assertEquals("Patrick",playerTeamWest2.getData().getName(), "Player must be Patrick");
 
-        TreeNode playerTeamEast1 = teamEast1.getChilds().get(0);
-        TreeNode playerTeamEast2 = teamEast2.getChilds().get(0);
         assertEquals("Giannis",playerTeamEast1.getData().getName(), "Player must be Giannis");
         assertEquals("Nickeil",playerTeamEast2.getData().getName(), "Player must be Nickeil");
     }

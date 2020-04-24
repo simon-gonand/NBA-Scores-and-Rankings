f612941c4d77920ab96c491ade84de6047b71559
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-04-24 18:26:30 +0100

    comment + set layouts

--- src/main/java/com/NBA_Rankings_Scores_Project/View/GamesListView.java
+++ src/main/java/com/NBA_Rankings_Scores_Project/View/GamesListView.java
@@ -7,16 +7,24 @@ import com.NBA_Rankings_Scores_Project.Tree.TreeGames;
 import com.NBA_Rankings_Scores_Project.Tree.TreeSeasonInfo;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * View that display all the games
+ */
 public class GamesListView{
     private JPanel panel;
     private TreeSeasonInfo info;
     private TreeGames treeGames;
 
+    /**
+     * Constructor that initialize the data members and call the function to display the game list button
+     * @param panel Panel where the content will be displayed
+     */
     public GamesListView (JPanel panel){
         this.panel = panel;
         panel.removeAll();
@@ -24,39 +32,39 @@ public class GamesListView{
         Parser parser = new Parser("src/main/resources/Season_19_20.xml");
         this.info = parser.getTreeSeason();
         this.treeGames = parser.getTreeSeasonGames(info);
+
         List<GameModel> games = treeGames.getAllGames();
-        int yPosButton = 0;
-        for (GameModel game : games) {
-            displayGame(game, yPosButton);
-            yPosButton += 100;
-        }
+        panel.setLayout(new GridLayout(6, 1));
+        for (GameModel game : games)
+            displayGame(game);
     }
 
-    private void displayGame(final GameModel game, int yPosButton){
+    /**
+     * Display the game list buttons
+     * @param game Game to display
+     */
+    private void displayGame(final GameModel game){
         JButton button = new JButton(game.getTotScore());
-        button.setBounds(0, yPosButton, panel.getWidth(), 100);
-        button.setLayout(null);
+        BorderLayout buttonLayout = new BorderLayout();
+        buttonLayout.preferredLayoutSize(this.panel);
+        button.setLayout(buttonLayout);
         this.panel.add(button);
 
         final Map<String, TeamModel> teams = treeGames.getTeamsOfGame(game);
 
         JLabel logoLeft = new JLabel(teams.get("Visitor").getName(),
                 new ImageIcon("src/main/resources/Icons/jersey.png"),
-                JLabel.LEFT);
+                JLabel.CENTER);
         JLabel logoRight = new JLabel(teams.get("Home").getName(),
-                new ImageIcon("src/main/resources/Icons/jersey.png"),
-                JLabel.RIGHT);
+                new ImageIcon("src/main/resources/Icons/jersey.png"), JLabel.CENTER);
 
         logoLeft.setVerticalTextPosition(JLabel.BOTTOM);
         logoLeft.setHorizontalTextPosition(JLabel.CENTER);
         logoRight.setVerticalTextPosition(JLabel.BOTTOM);
         logoRight.setHorizontalTextPosition(JLabel.CENTER);
 
-        logoLeft.setBounds(10, 0, 500, 100);
-        logoRight.setBounds(0, 0, button.getWidth() - 10, 100);
-
-        button.add(logoLeft);
-        button.add(logoRight);
+        button.add(logoLeft,BorderLayout.LINE_START);
+        button.add(logoRight, BorderLayout.LINE_END);
 
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
